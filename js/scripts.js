73f595060c006f981e550f0f98b547ee09ad1356
Mark Cotton <tifosigoon@gmail.com>
2012-08-13 16:33:55 -0500

    More changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,108 @@
 $(document).ready(function() {
-	
-	// global variables :(
-	user_id = ''
+    
+    // global variables :(
+    server = '/future/qliqserver'
+    user_lat = null
+    user_lng = null
 
-	// Show the login modal
-	$('#loginModal').modal({
-		backdrop:	'static',
-		keyboard:	false,
-		show:		true
-		})
-		
-	// Login form queries the API
-	// and closes the box if successful
-	$('#loginbutton').click(function() {
-		$.post('/qliqserver/login', {
-									'username': $('#loginUser').val(), 
-									'password':$('#loginPass').val()
-									}, function(data) {
+    // Show the login modal
+    if(!sessionStorage.getItem('user_id')) {
+        $('#loginModal').modal({
+            backdrop:   'static',
+            keyboard:   false,
+            show:       true
+        })
+    } else {
+        user_id = sessionStorage.getItem('user_id')
+        loadContentPane()   
+        loadBadgesPane()
+    }
+
+
+    // Check for geolocation
+    if ("geolocation" in navigator) {
+        console.log('geolocation is available')
+        navigator.geolocation.getCurrentPosition(function(position) {
+            sessionStorage.setItem('lat', position.coords.latitude)
+            sessionStorage.setItem('lng', position.coords.longitude)
+            user_lat = sessionStorage.getItem('lat')
+            user_lng = sessionStorage.getItem('lng')
+            waitForGeolocation()    
+        })
+    } else {
+        console.log('geolocation is not available')
+    }
+        
+    // Login form queries the API
+    // and closes the box if successful
+    $('#loginbutton').click(function() {
+        $.post(server + '/login', {
+                                    'username': $('#loginUser').val(), 
+                                    'password':$('#loginPass').val()
+                                    }, function(data) {
  
-                        user_id = data.data.user.id
+                        sessionStorage.setItem('user_id', data.data.user.id)
                         $('#loginModal').modal('hide')
+                        loadContentPane()
+                        loadBadgesPane()
                     })
-	})
+    })
                     
     // Once they've made it throught the login modal
     // We can use the user_id and start fetching
     // user data to fill these panes
-    $('#loginModal').on('hidden', function() {
-		// Load content for social
-		$.get('/qliqserver/feed', function(data) {
-			$('#social_pane').html('')	    
-			$('#social_pane').append('<ul></ul>')
-            for(var i=0; i< data.data.unread; i++) {
-            	$('#social_pane').append('<li>' + data.data.events[i].third_person_message + '</li>')
+    function loadContentPane() {
+        // Load content for social
+        $.get(server + '/feed', function(data) {
+            $('#social_pane').html('')      
+            $('#social_pane').append('<ul></ul>')
+            for(var i=0; i< data.data.events.length; i++) {
+                $('#social_pane ul').append('<li>' + data.data.events[i].third_person_message + '</li>')
                                                 }
-                                        })	
-		// Load content for places
-		$('#places_pane').html('')	    
-		
-		// Load content for deals
-		$('#deals_pane').html('')	    
-	    
-	    
-    })
+                                        })  
+    }
 
-		
+    function loadBadgesPane() {
+        // Load content for badges
+
+        $.get(server + '/badges/' + user_id, function(data) {
+        //  $('#badges_pane').html('')      
+        //  $('#badges_pane').append('<ul id="won_true">Unlocked Badges</ul>')
+        //  $('#badges_pane').append('<ul id="won_false">Locked Badges</ul>')
+            console.log(data.data.badges.length)        
+            for(var i=0; i< data.data.badges.length; i++) {
+        if(data.data.badges[i].won)  {
+            $('#won_true').append('<li>' + data.data.badges[i].name + '</li>')
+        } else {
+            $('#won_false').append('<li>' + data.data.badges[i].name + '</li>')
+        }
+            }
+        })      
+
+    }
+
+    function waitForGeolocation() {
+
+        // Load content for places
+        $.get(server + '/places?lat=' + user_lat + '&lng=' + user_lng, function(data) {
+            $('#places_pane').html('')
+            $('#places_pane').append('<ul></ul>')
+            for(var i=0; i< data.data.results.local.length; i++)  {
+                $.get(server + '/location/' + data.data.results.local[i].location_id, function(data) {
+                    $('#places_pane ul').append('<li>' + data.data.location.name + ' - ' + data.data.location.street_1 + '</li>')
+                })
+            }
+        })
+
+    
+        // Load content for deals
+        $('#deals_pane').html('')       
+        
+        
+    }
+
+        
 
 
-	
+    
 })
